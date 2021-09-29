53352b4886b3fd2d93b088148e3049cd77bd02fb
faeb187 <fabio@ambrite.ch>
2021-09-29 17:45:55 +0200

    ANKH#6 stylus loaded by webpack now

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#clean up
+# cleaning
 rm dist/*.js
 rm dist/index.html
 rm -r dist/assets/css
@@ -8,16 +8,7 @@ rm -r dist/assets/js
 
 # deployment
 npm run build
-
-# @todo webpack stylus
-# coffee -c -b gulpfile.coffee
-gulp styl
-# rm gulpfile.js
-
-# provide index.html
 cp src/index.html dist
-
-# serve app
 npx http-server ./dist --proxy http://localhost:8080?
 
 exit 0
